b0faa51371c6fef22e9f9f4949413cc0c844667d
rapampamfau <rafal.szczepanczyk7@gmail.com>
2023-11-26 00:46:21 +0100

    feat: downloading a file from page

---
+++ src/main/java/org/cryptopriceanalyzer/service/DataFetcher.java
@@ -0,0 +1,32 @@
+package org.cryptopriceanalyzer.service;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class DataFetcher {
+
+    private static final String YAHOO_FINANCE = "https://finance.yahoo.com/quote/BTC-USD/history/?guccounter=1";
+
+    public void downloadData(WebDriver driver) {
+        try {
+            driver.get(YAHOO_FINANCE);
+
+            WebElement rejectCookies = driver.findElement(By.name("reject"));
+            rejectCookies.click();
+            Thread.sleep(1000);
+
+            WebElement here = driver.findElement(By.linkText("here"));
+            here.click();
+
+            WebElement downloadButton = driver.findElement(By.linkText("Download"));
+            downloadButton.click();
+            Thread.sleep(1000);
+
+        } catch (InterruptedException e) {
+            System.out.println("Error:" + e.getMessage());
+        } finally {
+            driver.quit();
+        }
+    }
+}
