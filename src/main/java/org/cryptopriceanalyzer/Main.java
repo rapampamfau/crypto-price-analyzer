e047a4d4f2fa990cc279d763554b4fc47eac8461
rapampamfau <rafal.szczepanczyk7@gmail.com>
2023-11-25 23:48:13 +0100

    fix: refactoring

--- src/main/java/org/cryptopriceanalyzer/Main.java
+++ src/main/java/org/cryptopriceanalyzer/Main.java
@@ -1,6 +1,12 @@
 package org.cryptopriceanalyzer;
 
+import org.cryptopriceanalyzer.config.WebDriverConfig;
+import org.openqa.selenium.WebDriver;
+
 public class Main {
     public static void main(String[] args) {
+
+        WebDriver wd = WebDriverConfig.getDriver();
+        wd.get("https://www.google.com/");
     }
 }
