72db33d1431dda6b20a21125616e742bf64d9e82
rapampamfau <rafal.szczepanczyk7@gmail.com>
2023-11-25 23:44:05 +0100

    added dependencies for SeleniumWebDriver

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,6 +12,8 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("org.seleniumhq.selenium:selenium-java:4.15.0")
+    implementation("org.seleniumhq.selenium:selenium-chrome-driver:4.15.0")
 }
 
 tasks.test {
